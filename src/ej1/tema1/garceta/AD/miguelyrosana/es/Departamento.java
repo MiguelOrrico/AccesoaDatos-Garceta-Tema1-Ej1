9de7f3b3865c1a11ba91b464a4608cf5186b568a
MiguelOrrico <otm@iesseveroochoa.net>
2015-07-24 20:56:01 +0200

    Añadida la clase Departamento

---
+++ src/ej1/tema1/garceta/AD/miguelyrosana/es/Departamento.java
@@ -0,0 +1,8 @@
+package ej1.tema1.garceta.AD.miguelyrosana.es;
+
+public class Departamento {
+	private int numeroDepartamento;
+	private String Nombre;
+	private String Localidad;
+
+}
---
+++ src/ej1/tema1/garceta/AD/miguelyrosana/es/FicheroBinarioDepartamentos.java
@@ -0,0 +1,7 @@
+package ej1.tema1.garceta.AD.miguelyrosana.es;
+
+public class FicheroBinarioDepartamentos {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
